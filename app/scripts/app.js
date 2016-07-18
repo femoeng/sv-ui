5e16b31c7bfdac9918af1c237cd7dfd27b32297a
Densque Joaquim Jamal <densquejamal@gmail.com>
2016-07-18 11:48:57 +0200

    Commit inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name svUiApp
+ * @description
+ * # svUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('svUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
