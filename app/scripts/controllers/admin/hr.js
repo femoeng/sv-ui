c466bbd3c9565f344850d325faf1f0cc0ee271ab
Densque Joaquim Jamal <densquejamal@gmail.com>
2016-09-23 10:29:39 +0200

    Implementação de registo de Visitantes

--- app/scripts/controllers/admin/hr.js
+++ app/scripts/controllers/admin/hr.js
@@ -9,18 +9,6 @@
  */
 angular.module('svUiApp')
   .controller('AdminHrCtrl', function ($scope, AdminService) {
-  	$scope.visitantes = [];
-  	AdminService.getVisitantes(function(res){ 
-  		$scope.visitantes = res.visitante; 
-  		console.log($scope.visitantes);
-  	}, function(err){ 
-  		console.log(err);
-  	} );
-  	$scope.createVisitor = function(visitor){
-  		console.log(visitor);
-  		console.log("Aqui ");
-  		AdminService.createVisitante(visitor,function(res){console.log(res)},function(err){console.log(err)});
-  	}
   	$scope.btnSelecionar = function(id){
   		console.log(id);
   		$(".btn-selecionado").removeClass("btn-selecionar");
--- app/scripts/controllers/admin/hr/visitors.js
+++ app/scripts/controllers/admin/hr/visitors.js
@@ -8,8 +8,27 @@
  * Controller of the svUiApp
  */
 angular.module('svUiApp')
-  .controller('AdminHrVisitorsCtrl', function ($scope) {
+  .controller('AdminHrVisitorsCtrl', function ($scope, AdminService) {
+    $scope.visitantes = [];
+    $scope.EditboxChecked = false;
+  	AdminService.getVisitantes(function(res){ 
+  		$scope.visitantes = res.visitante; 
+  		console.log($scope.visitantes);
+  	}, function(err){ 
+  		console.log(err);
+  	} );
+
+  	$scope.createVisitor = function(visitor){
+  		AdminService.createVisitante(visitor,function(res){
+  			$scope.visitantes.push(visitor);
+  			console.log(res)
+  		},function(err) {
+  			console.log(err)
+  		});
+  	}
+  	
     $scope.enableVisitorEdit = function() {
-    	
+    	$scope.EditboxChecked = true;
     }
+
   });
